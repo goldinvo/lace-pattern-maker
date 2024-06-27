1631557304127528eb7b7be20d85ce0e0adc30f2
goldinvo <goldinbaokimvo@gmail.com>
2024-06-27 12:37:21 -0500

    initial canvas; zoom and pan

--- src/FabricCanvas.jsx
+++ src/FabricCanvas.jsx
@@ -1,59 +1,77 @@
 import React, { useRef, useEffect, useState } from 'react'
 import { fabric } from 'fabric'
 
-// export default function FabricCanvas() {
-//   const canvasRef = useRef(null);
-  
-//   useEffect(() => {
-//     const canvas = new fabric.Canvas(canvasRef.current);
 
-//     // Add a rectangle to the canvas
-//     const rect = new fabric.Rect({
-//       left: 100,
-//       top: 100,
-//       fill: 'red',
-//       width: 200,
-//       height: 200,
-//     });
-//     canvas.add(rect);
+function fabricInit(canvas) {
+  // Canvas zoom
+  canvas.on('mouse:wheel', function(opt) {
+      var delta = opt.e.deltaY;
+      var zoom = canvas.getZoom();
+      zoom *= .999 ** delta;
+      if (zoom > 20) zoom = 20;
+      if (zoom < 0.01) zoom = 0.01;
+      canvas.zoomToPoint({ x: opt.e.offsetX, y: opt.e.offsetY }, zoom);
+      opt.e.preventDefault();
+      opt.e.stopPropagation();
+  });
 
-//     // Clean up the canvas when the component is unmounted
-//     return () => {
-//       canvas.dispose();
-//     };
-//   }, []);
-  
-//   return <canvas ref={canvasRef} width={600} height={400} />;
-// }
+  // Canvas pan
+  canvas.on('mouse:down', function(opt) {
+    var evt = opt.e;
+    console.log(opt);
+    if (evt.altKey === true) {
+      this.isDragging = true;
+      this.selection = false;
+      this.lastPosX = evt.clientX;
+      this.lastPosY = evt.clientY;
+    }
+  });
+  canvas.on('mouse:move', function(opt) {
+    if (this.isDragging) {
+      var e = opt.e;
+      var vpt = this.viewportTransform;
+      vpt[4] += e.clientX - this.lastPosX;
+      vpt[5] += e.clientY - this.lastPosY;
+      this.requestRenderAll();
+      this.lastPosX = e.clientX;
+      this.lastPosY = e.clientY;
+    }
+  });
+  canvas.on('mouse:up', function(opt) {
+    // on mouse up we want to recalculate new interaction
+    // for all objects, so we call setViewportTransform
+    this.setViewportTransform(this.viewportTransform);
+    this.isDragging = false;
+    this.selection = true;
+  });
+}
 
-const FabricCanvas = () => {
+export default function FabricCanvas() {
   const canvasRef = useRef(null);
-  const fabricCanvasRef = useRef(null);
+  const fabricRef = useRef(null);
   const [selectedObject, setSelectedObject] = useState(null);
 
   useEffect(() => {
-    // Initialize the Fabric.js canvas
-    fabricCanvasRef.current = new fabric.Canvas(canvasRef.current);
-
-    // Example: Add a rectangle to the canvas
-    const rect = new fabric.Rect({
-      left: 100,
-      top: 100,
-      fill: 'red',
-      width: 200,
-      height: 200,
+    fabricRef.current = new fabric.Canvas(canvasRef.current, {
+      backgroundColor: "#ffffff",
+    });
+    fabricInit(fabricRef.current);    
+    // Add event handler for object selection
+    fabricRef.current.on('selection:created', (e) => {
+      setSelectedObject(e.selected[0]);
     });
 
-    fabricCanvasRef.current.add(rect);
+    fabricRef.current.on('selection:updated', (e) => {
+      setSelectedObject(e.selected[0]);
+    });
 
-    // Add event handler for object selection
-    fabricCanvasRef.current.on('object:selected', (e) => {
-      setSelectedObject(e.target);
+    fabricRef.current.on('selection:cleared', () => {
+      setSelectedObject(null);
     });
 
     // Cleanup on unmount
     return () => {
-      fabricCanvasRef.current.dispose();
+      fabricRef.current.dispose();
     };
   }, []);
 
@@ -64,19 +82,21 @@ const FabricCanvas = () => {
       left: 150,
       top: 150,
     });
-    fabricCanvasRef.current.add(circle);
+    fabricRef.current.add(circle);
   };
 
   const deleteSelectedObject = () => {
     if (selectedObject) {
-      fabricCanvasRef.current.remove(selectedObject);
+      fabricRef.current.remove(selectedObject);
       setSelectedObject(null);
     }
   };
 
   return (
     <div>
-      <canvas ref={canvasRef} width="800" height="600" />
+      <canvas ref={canvasRef} width="800" height="600"> 
+        Could not load canvas. Please update browser or enable JavaScript.
+      </canvas>
       <button onClick={addCircle}>Add Circle</button>
       <button onClick={deleteSelectedObject} disabled={!selectedObject}>
         Delete Selected Object
@@ -84,5 +104,3 @@ const FabricCanvas = () => {
     </div>
   );
 };
-
-export default FabricCanvas;
