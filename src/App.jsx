7b6dcaced39e1a23cf3bbf34ff1210181c4c3c0c
goldinvo <goldinbaokimvo@gmail.com>
2024-06-27 21:07:54 -0500

    grid

--- src/App.jsx
+++ src/App.jsx
@@ -3,21 +3,77 @@ import { fabric } from 'fabric'
 import Toolbar from './Toolbar.jsx'
 import Header from './Header.jsx'
 
+const GRID_COLOR = "#000000";
+const GRID_OPACITY = 0.3;
+const CELL_SIZE = 20;
+
+const MAX_SCALE = 3;
+const MIN_SCALE = 0.3;
+
+
 function App() {
   const canvasRef = useRef(null);
   const fabRef = useRef(null);
-  const fabStateRef = useRef({
-    mode: 'select'
-  });
 
   const [curPos, setCurPos] = useState({x: 0, y: 0});
   const [mode, setMode] = useState('select');
 
   useEffect(() => {
-    // Init fabric
+    // Initialize fabric
     fabRef.current = new fabric.Canvas(canvasRef.current, {
-      backgroundColor: "#eeeeee",
+      backgroundColor: "#fefefe",
+      state: {},
     });
+
+    // Initialize grid https://stackoverflow.com/questions/68604136/fabric-js-canvas-infinite-background-grid-like-miro
+    var infBGrid = fabric.util.createClass(fabric.Object, { 
+      type: 'infBGrid',
+      
+      initialize: function () {
+
+      },
+      
+      render: function (ctx) {
+          let canvas = fabRef.current;
+          let zoom = canvas.getZoom();
+          let offX = canvas.viewportTransform[4];
+          let offY = canvas.viewportTransform[5];
+  
+          ctx.save();
+          ctx.strokeStyle = "#dddddd";
+          ctx.lineWidth = 1;
+  
+          let gridSize = CELL_SIZE * zoom;
+  
+          const numCellsX = Math.ceil(canvas.width / gridSize);
+          const numCellsY = Math.ceil(canvas.height / gridSize);
+  
+          let gridOffsetX = offX % gridSize;
+          let gridOffsetY = offY % gridSize;
+          ctx.beginPath();
+          // draw vertical lines
+          for (let i = 0; i <= numCellsX; i++) {
+            let x = gridOffsetX + i * gridSize;
+            ctx.moveTo((x - offX) / zoom, (0 - offY) / zoom);
+            ctx.lineTo((x - offX) / zoom, (canvas.height - offY) / zoom);
+          }
+        
+          // draw horizontal lines
+          for (let i = 0; i <= numCellsY; i++) {
+            let y = gridOffsetY + i * gridSize;
+            ctx.moveTo((0 - offX) / zoom, (y - offY) / zoom);
+            ctx.lineTo((canvas.width - offX) / zoom, (y - offY) / zoom);
+          }
+        
+          ctx.stroke();
+          ctx.closePath();
+          ctx.restore();
+      }
+    });
+  
+    let bg = new infBGrid();
+    fabRef.current.add(bg);
+    fabRef.current.renderAll();
 
     // Fabric events handlers
     fabRef.current.on('mouse:wheel', handleScroll);
@@ -32,25 +88,23 @@ function App() {
   }, []);
 
   function handleScroll(opt) {
-    var delta = opt.e.deltaY;
     var zoom = fabRef.current.getZoom();
-    zoom *= .999 ** delta;
-    if (zoom > 20) zoom = 20;
-    if (zoom < 0.01) zoom = 0.01;
+    zoom *= .999 ** opt.e.deltaY;
+    if (zoom > MAX_SCALE) zoom = MAX_SCALE;
+    if (zoom < MIN_SCALE) zoom = MIN_SCALE;
     fabRef.current.zoomToPoint({ x: opt.e.offsetX, y: opt.e.offsetY }, zoom);
     opt.e.preventDefault();
     opt.e.stopPropagation();
   }
 
   function handleMouseDown(opt) {
-    console.log(fabStateRef)
     var evt = opt.e;
     if (evt.altKey === true) {
-      fabStateRef.current.isDragging = true;
+      fabRef.current.state.isDragging = true;
       fabRef.current.selection = false;
-      fabStateRef.current.lastPosX = evt.clientX;
-      fabStateRef.current.lastPosY = evt.clientY;
-    } else if (fabStateRef.current.mode == 'draw') {
+      fabRef.current.state.lastPosX = evt.clientX;
+      fabRef.current.state.lastPosY = evt.clientY;
+    } else if (fabRef.current.state.mode == 'draw') {
       const circle = new fabric.Circle({
         radius: 5,
         fill: 'black',
@@ -64,14 +118,13 @@ function App() {
   function handleMouseMove(opt) {
     setCurPos(opt.absolutePointer)
 
-    if (fabStateRef.current.isDragging) {
-      var e = opt.e;
+    if (fabRef.current.state.isDragging) {
       var vpt = fabRef.current.viewportTransform;
-      vpt[4] += e.clientX - fabStateRef.current.lastPosX;
-      vpt[5] += e.clientY - fabStateRef.current.lastPosY;
+      vpt[4] += opt.e.clientX - fabRef.current.state.lastPosX;
+      vpt[5] += opt.e.clientY - fabRef.current.state.lastPosY;
       fabRef.current.requestRenderAll();
-      fabStateRef.current.lastPosX = e.clientX;
-      fabStateRef.current.lastPosY = e.clientY;
+      fabRef.current.state.lastPosX = opt.e.clientX;
+      fabRef.current.state.lastPosY = opt.e.clientY;
     }
   }
 
@@ -79,13 +132,13 @@ function App() {
     // on mouse up we want to recalculate new interaction
     // for all objects, so we call setViewportTransform
     fabRef.current.setViewportTransform(fabRef.current.viewportTransform);
-    fabStateRef.current.isDragging = false;
+    fabRef.current.state.isDragging = false;
     fabRef.current.selection = true;
   }
 
   function handleMode(event, newMode){
     if (newMode !== null){
-      fabStateRef.current.mode = newMode;
+      fabRef.current.state.mode = newMode;
       setMode(newMode);
     }
   };
--- src/README.md
+++ src/README.md
@@ -1,6 +1,6 @@
 # Rough Documentation
 
-Fabric.js integration with React is somewhat limited. I work around this by keeping fabRef and fabStateRef to the fabric instance and fabric state respectively.
+Fabric.js integration with React is somewhat limited. I work around this by keeping a fabRef reference to the fabric and storing its state in fabRef.current.state (to avoid name conflicts).
 
 Any state that needs to be shared with React UI components has a corresponding useState call. Any updates to state **must** update both React state and Fabric canvas state.
 
