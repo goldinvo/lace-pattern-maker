491c97d73e4d6274dbb3eca71cc63e9d77623bda
goldinvo <goldinbaokimvo@gmail.com>
2024-07-09 17:55:30 -0500

    about dialog redo

--- src/header/AboutDialog.jsx
+++ src/header/AboutDialog.jsx
@@ -1,1 +1,87 @@
-// TODO: make the about Dialog its own component
+import Dialog from '@mui/material/Dialog';
+import DialogTitle from '@mui/material/DialogTitle';
+import DialogContent from '@mui/material/DialogContent';
+import DialogActions from '@mui/material/DialogActions';
+import IconButton from '@mui/material/IconButton';
+import CloseIcon from '@mui/icons-material/Close';
+import Button from '@mui/material/Button';
+
+/*
+
+# Lace Pattern Tool
+**Lace Pattern Tool** is an interactive canvas designed for making bobbin lace 
+patterns. It is easy-to-learn and free-to-use!
+
+If you enjoyed using this tool or have any feedback, bug reports, or feature requests, please 
+email me at goldin@goldinvo.com. 
+
+## Quick Start
+Hover over buttons to see the actions they perform and the keyboard shortcuts associated with them.
+1. Navigate the canvas by zooming (scroll) and panning (Pan mode).
+2. Use Draw mode to draw points, lines, and freehand draw.
+3. Use Select mode to make a selection, either by using the default Box select or by using Lasso select.
+With select mode you can Group Move, Group Delete, cut/copy/paste, or perform transformations.
+    - To use many of these features, you must first plot a **"Control Point"** by double clicking.
+4. Export/Import a text representation of the canvas, or Save/Load to your browser's local storage (there is only one save file!),
+5. Print your finished pattern at the correct scale using the Print Dialog form. 
+
+## Further Information
+Visit my [blog post](https://goldinvo.com/blog/2024/06/29/lace-pattern-tool) details about
+using this tool, or watch the video below.
+
+*/
+
+function AboutDialog({ open, handleClose }) {
+ 
+  return (
+    <Dialog open={open} onClose={handleClose} maxWidth="md" fullWidth>
+          <DialogTitle>
+            About
+            <IconButton
+              aria-label="close"
+              onClick={handleClose}
+              sx={{
+                position: 'absolute',
+                right: 8,
+                top: 8,
+                color: (theme) => theme.palette.grey[500],
+              }}
+            >
+              <CloseIcon />
+            </IconButton>
+          </DialogTitle>
+          <DialogContent dividers>
+          <h1>Lace Pattern Tool</h1>
+    <p><strong>Lace Pattern Tool</strong> is an interactive canvas designed for making bobbin lace patterns. It is easy-to-learn and free-to-use!</p>
+
+    <p>If you enjoyed using this tool or have any feedback, bug reports, or feature requests, please email me at <a href="mailto:goldin@goldinvo.com">goldin@goldinvo.com</a>.</p>
+
+    <h2>Quick Start</h2>
+    <p>Hover over buttons to see the actions they perform and the keyboard shortcuts associated with them.</p>
+    <ol>
+        <li>Navigate the canvas by zooming (scroll) and panning (Pan mode).</li>
+        <li>Use Draw mode to draw points, lines, and freehand draw.</li>
+        <li>Use Select mode to make a selection, either by using the default Box select or by using Lasso select. With select mode you can Group Move, Group Delete, cut/copy/paste, or perform transformations.
+            <ul>
+                <li>To use many of these features, you must first plot a <strong>"Control Point"</strong> by double-clicking.</li>
+            </ul>
+        </li>
+        <li>Export/Import a text representation of the canvas, or Save/Load to your browser's local storage (there is only one save file!).</li>
+        <li>Print your finished pattern at the correct scale using the Print Dialog form.</li>
+    </ol>
+
+    <h2>Further Information</h2>
+    <p>Visit my <a href="https://goldinvo.com/blog/2024/06/29/lace-pattern-tool">blog post</a> for details about using this tool, or watch the video below.</p>
+          </DialogContent>
+          <DialogActions>
+            <Button onClick={handleClose} color="primary">
+              Close
+            </Button>
+          </DialogActions>
+        </Dialog>
+  );
+}
+
+export default AboutDialog;
+
+
--- src/header/Header.jsx
+++ src/header/Header.jsx
@@ -5,19 +5,8 @@ import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
 import PrintDialog from './PrintDialog';
 import ExportImportDialog from './ExportImportDialog';
-import Dialog from '@mui/material/Dialog';
-import DialogTitle from '@mui/material/DialogTitle';
-import DialogContent from '@mui/material/DialogContent';
-import DialogActions from '@mui/material/DialogActions';
+import AboutDialog from './AboutDialog';
 import Box from '@mui/material/Box';
-import IconButton from '@mui/material/IconButton';
-import CloseIcon from '@mui/icons-material/Close';
-import Link from '@mui/material/Link';
-import List from '@mui/material/List';
-import ListItem from '@mui/material/ListItem';
-import ListItemText from '@mui/material/ListItemText';
-import ListItemIcon from '@mui/material/ListItemIcon';
-import ArrowRightIcon from '@mui/icons-material/ArrowRight';
 import Tooltip from '@mui/material/Tooltip';
 import Snackbar from '@mui/material/Snackbar';
 import Alert from '@mui/material/Alert';
@@ -127,100 +116,10 @@ function Header(props) {
           handleClose={() => setPrintOpen(false)}
           handlePrint={props.handlePrint}
         />
-        <Dialog open={aboutOpen} onClose={() => setAboutOpen(false)} maxWidth="md" fullWidth>
-          <DialogTitle>
-            About
-            <IconButton
-              aria-label="close"
-              onClick={() => setAboutOpen(false)}
-              sx={{
-                position: 'absolute',
-                right: 8,
-                top: 8,
-                color: (theme) => theme.palette.grey[500],
-              }}
-            >
-              <CloseIcon />
-            </IconButton>
-          </DialogTitle>
-          <DialogContent dividers>
-            <Box>
-              <Typography variant="h4" gutterBottom>
-                Lace Pattern Tool
-              </Typography>
-              <Typography variant="body1" paragraph>
-                <strong>Lace Pattern Tool</strong> is an interactive canvas designed for making bobbin lace patterns. It is easy-to-learn and free-to-use!
-              </Typography>
-              <Typography variant="body1" paragraph>
-                Read below to get the basics, or visit my{' '}
-                <Link href="https://goldinvo.com/blog/2024/06/29/lace-pattern-tool" target="_blank" rel="noopener">
-                  blog post
-                </Link>{' '}
-                to learn more about this tool. If you enjoyed using this tool or have any feedback, bug reports, or feature requests, please email me at{' '}
-                <Link href="mailto:goldin@goldinvo.com">goldin@goldinvo.com</Link>.
-              </Typography>
-              <Typography variant="h5" gutterBottom>
-                Usage
-              </Typography>
-              <Typography variant="h6" gutterBottom>
-                Navigation
-              </Typography>
-              <List>
-                <ListItem>
-                  <ListItemIcon>
-                    <ArrowRightIcon />
-                  </ListItemIcon>
-                  <ListItemText primary="Scroll to zoom in and out" />
-                </ListItem>
-                <ListItem>
-                  <ListItemIcon>
-                    <ArrowRightIcon />
-                  </ListItemIcon>
-                  <ListItemText primary="Hold Alt or use Pan mode to move the canvas" />
-                </ListItem>
-              </List>
-              <Typography variant="h6" gutterBottom>
-                Working with Objects
-              </Typography>
-              <List>
-                <ListItem>
-                  <ListItemIcon>
-                    <ArrowRightIcon />
-                  </ListItemIcon>
-                  <ListItemText primary="In Draw mode, you can plot points, curves, or freehand draw. Holding shift allows you to draw lines in freehand draw mode." />
-                </ListItem>
-                <ListItem>
-                  <ListItemIcon>
-                    <ArrowRightIcon />
-                  </ListItemIcon>
-                  <ListItemText primary="Use Erase mode to delete objects, or delete a selection in Select mode." />
-                </ListItem>
-              </List>
-              <Typography variant="h6" gutterBottom>
-                Using Select Mode
-              </Typography>
-              <List>
-                <ListItem>
-                  <ListItemIcon>
-                    <ArrowRightIcon />
-                  </ListItemIcon>
-                  <ListItemText primary="Click or click-and-drag to make a selection. Shift-click to modify the selection." />
-                </ListItem>
-                <ListItem>
-                  <ListItemIcon>
-                    <ArrowRightIcon />
-                  </ListItemIcon>
-                  <ListItemText primary='To copy and paste, you need to have a blue "control point" plotted. Objects are copy/pasted relative to the control point. Double click to plot a control point.' />
-                </ListItem>
-              </List>
-            </Box>
-          </DialogContent>
-          <DialogActions>
-            <Button onClick={() => setAboutOpen(false)} color="primary">
-              Close
-            </Button>
-          </DialogActions>
-        </Dialog>
+        <AboutDialog 
+          open={aboutOpen}
+          handleClose={() => {setAboutOpen(false)}}
+        />
       </Toolbar>
       <Snackbar
         open={snackbarOpen}
@@ -238,24 +137,3 @@ function Header(props) {
 
 export default Header;
 
-/*
-# Lace Pattern Tool
-**Lace Pattern Tool** is an interactive canvas designed for making bobbin lace 
-patterns. It is easy-to-learn and free-to-use!
-
-Read below to get the basics, or visit my [blog post](https://goldinvo.com/blog/2024/06/29/lace-pattern-tool)
-to learn more about this tool. If you enjoyed using this tool or have any feedback, bug reports, or feature requests, please 
-email me at goldin@goldinvo.com. 
-
-## Usage
-1. Navigation
-  - Scroll to zoom in and out
-  - Hold Alt or use Pan mode to move the canvas
-2. Working with Objects
-  - In Draw mode, you can plot points, curves, or freehand draw. Holding shift allows you to draw lines in freehand draw mode.
-  - Use Erase mode to delete objects, or delete a selection in Select mode.
-3. Using Select Mode
-  - Click or click-and-drag to make a selection. Shift-click to modify the selection.
-  - To copy and paste, you need to have a blue "control point" plotted. Objects are copy/pasted relative to the control point. Double click to plot a control point.
-
-*/
